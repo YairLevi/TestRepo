e1824e2b4f7d64c92e14d634cddfb92650c9d736
Yair Levi <yairlevi2001@gmail.com>
2022-03-14 19:25:43 +0200

    sheesh3

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 function YO5() {
     console.log('this is a function')
 }
-<<<<<<< Updated upstream
+
 function BRUH() {
     console.log('this is bruh function')
-=======
-
-function YO() {
     console.log('hello')
     console.log('this is yo function')
->>>>>>> Stashed changes
 }
